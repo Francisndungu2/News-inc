278cf80f434194cf4279efb3bb96b4af46a786fd
Francisndungu2 <francianmbugua@gmail.com>
2020-12-13 16:20:12 +0300

    add settergetter

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -15,4 +15,53 @@ public class Department {
         this.departmentName = departmentName;
         this.description = description;
     }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public List<User> getDepartmentUsers() {
+        return departmentUsers;
+    }
+
+    public void setDepartmentUsers(List<User> departmentUsers) {
+        this.departmentUsers = departmentUsers;
+    }
+
+    public int getNoOfUsers() {
+        return noOfUsers;
+    }
+
+    public void setNoOfUsers(int noOfUsers) {
+        this.noOfUsers = noOfUsers;
+    }
+
+    public List<DepartmentNews> getDepartmentNews() {
+        return departmentNews;
+    }
+
+    public void setDepartmentNews(List<DepartmentNews> departmentNews) {
+        this.departmentNews = departmentNews;
+    }
 }
+
+
