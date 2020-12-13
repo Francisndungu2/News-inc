e42b6be26f7091388cba28d7715618cbc6ce1b9e
Francisndungu2 <francianmbugua@gmail.com>
2020-12-13 16:16:44 +0300

    departmentcontructor

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,18 @@
 package models;
 
+import java.util.List;
+
 public class Department {
+    private int id;
+    private String departmentName;
+    private String description;
+    private List<User> departmentUsers;
+    private int noOfUsers;
+    private List<DepartmentNews> departmentNews;
+
+
+    public Department(String departmentName, String description) {
+        this.departmentName = departmentName;
+        this.description = description;
+    }
 }
