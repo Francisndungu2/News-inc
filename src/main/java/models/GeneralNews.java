7ab8768653acf9ecba2f8ec74e308c657d0fdeb2
Francisndungu2 <francianmbugua@gmail.com>
2020-12-13 16:30:46 +0300

    add title,content

--- src/main/java/models/GeneralNews.java
+++ src/main/java/models/GeneralNews.java
@@ -1,4 +1,12 @@
 package models;
 
 public class GeneralNews {
+    private String title;
+    private int id;
+    private String content;
+
+    public GeneralNews(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
