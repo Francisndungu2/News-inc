0342d8cd9ae1e9d3e05d98673e30afa0a1b9af7f
Francisndungu2 <francianmbugua@gmail.com>
2020-12-14 22:56:01 +0300

    addreadmefile

--- src/main/java/DAO/UserDao.java
+++ src/main/java/DAO/UserDao.java
@@ -1,6 +1,14 @@
 package DAO;
 
-public interface UserDao {
+import models.User;
 
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+
+    List<User> getAllUser();
+
+    User getUserById(int id);
 
 }
